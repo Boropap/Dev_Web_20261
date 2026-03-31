6a1265aaf5db86eb7360bbf9f3ac2908ef8ac253
Caio Monteiro Rangel <20212BSI0022@cefetes.br>
2026-03-31 12:34:02 -0300

    Adding logic function using php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$idade =5;
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -8,7 +13,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Testando PHP</h1>
+<div class="container">
+    <?php
+        if($idade <= 18){
+            echo "<h1>Você não pode dirigir</h1>";
+            echo "<h1>$idade anos</h1><hr>";
+        }else{
+            echo "<h1>Você pode dirigir</h1>";
+            echo "<h1>$idade</h1><hr>";
+        }
+        
+        
+    ?>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
