cdab4d2c20058cda43b35b7d98ab2be719fe730a
caior <caio.rangel2003@gmail.com>
2026-04-16 20:36:44 -0300

    Organizing different websites in the repository

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$idade =5;
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula PHP</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <?php
+        if($idade <= 18){
+            echo "<h1>Você não pode dirigir</h1>";
+            echo "<h1>$idade anos</h1><hr>";
+        }else{
+            echo "<h1>Você pode dirigir</h1>";
+            echo "<h1>$idade</h1><hr>";
+        }
+        
+        
+    ?>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
